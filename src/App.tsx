12622fe01b1ec8220ea5c81b0faa7c0951fe75f9
psbrunosouza <psbrunosouza@gmail.com>
2021-07-18 21:21:11 -0300

    layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "rsuite/dist/styles/rsuite-default.css";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  Sidebar,
+  Navbar,
+  Nav,
+  Dropdown,
+  Icon,
+} from "rsuite";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <Navbar>
+          <Navbar.Header>
+            <a href="#a" className="navbar-brand logo">
+              RSUITE
+            </a>
+          </Navbar.Header>
+          <Navbar.Body>
+            <Nav>
+              <Nav.Item icon={<Icon icon="home" />}>Home</Nav.Item>
+              <Nav.Item>News</Nav.Item>
+              <Nav.Item>Products</Nav.Item>
+              <Dropdown title="About">
+                <Dropdown.Item>Company</Dropdown.Item>
+                <Dropdown.Item>Team</Dropdown.Item>
+                <Dropdown.Item>Contact</Dropdown.Item>
+              </Dropdown>
+            </Nav>
+            <Nav pullRight>
+              <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+            </Nav>
+          </Navbar.Body>
+        </Navbar>
+      </Header>
+      <Container>
+        <Sidebar>Sidebar</Sidebar>
+        <Content>Content</Content>
+      </Container>
+      <Footer>Footer</Footer>
+    </Container>
   );
 }
 
