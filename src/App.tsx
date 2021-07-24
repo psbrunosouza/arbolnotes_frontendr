a6f1ae670a3911094482bddb786bd38012c88475
psbrunosouza <psbrunosouza@gmail.com>
2021-07-24 18:58:59 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
 import React from "react";
-import "rsuite/dist/styles/rsuite-default.css";
-import {
-  Container,
-  Header,
-  Content,
-  Footer,
-  Sidebar,
-  Navbar,
-  Nav,
-  Icon,
-} from "rsuite";
 
 function App() {
   return (
-    <Container>
-      <Header>
-        <Navbar>
-          <Navbar.Header>
-            <a href="#a" className="navbar-brand logo">
-              ARBOL
-            </a>
-          </Navbar.Header>
-          <Navbar.Body>
-            <Nav>
-              <Nav.Item icon={<Icon icon="home" />}>Dashboard</Nav.Item>
-            </Nav>
-            <Nav pullRight>
-              <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
-            </Nav>
-          </Navbar.Body>
-        </Navbar>
-      </Header>
-      <Container>
-        <Sidebar>Sidebar</Sidebar>
-        <Content>Content</Content>
-      </Container>
-      <Footer>Footer</Footer>
-    </Container>
+    <div></div>
   );
 }
 
