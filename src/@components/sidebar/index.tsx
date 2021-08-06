89ec90e265dd9065bc1c7c21e8fd972d2e3221a2
psbrunosouza <psbrunosouza@gmail.com>
2021-08-06 16:52:32 -0300

    some improvements

--- src/@components/sidebar/index.tsx
+++ src/@components/sidebar/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Sidenav, Nav, Icon, Dropdown } from "rsuite";
 import "./style.less";
 
-export function SidebarNav() {
+export function SidenavComponent() {
   return (
     <div className="navbar">
-      <Sidenav>
+      <Sidenav expanded={false}>
         <Sidenav.Body>
           <Nav>
             <Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
@@ -17,11 +17,10 @@ export function SidebarNav() {
               title="Workspaces"
               icon={<Icon icon="pagelines" />}
             >
-              <Dropdown
-                eventKey="3"
-                title="Categories"
-                icon={<Icon icon="gear-circle" />}
-              ></Dropdown>
+              <Dropdown.Item divider className="divider"/>
+              <Dropdown.Item className="panelStyles" panel>
+                category1
+              </Dropdown.Item>
             </Dropdown>
           </Nav>
         </Sidenav.Body>
--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,46 +1,28 @@
 import React from "react";
 import {
-  Navbar,
-  Nav,
-  Icon,
   Container,
   Header,
   Footer,
   Content,
   Sidebar,
 } from "rsuite";
-import {SidebarNav} from '../../@components/sidebar';
-import './style.less';
+import { SidenavComponent } from "../../@components/sidebar";
+import { NavbarComponent } from "../../@components/navbar";
+import "./style.less";
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <Container className="container">
       <Header>
-        <Navbar appearance="inverse">
-          <Navbar.Header>
-            <a href="#a" className="navbar-brand logo">
-              REDO
-            </a>
-          </Navbar.Header>
-          <Navbar.Body>
-            <Nav>
-              <Nav.Item icon={<Icon icon="dashboard" />}>Dashboard</Nav.Item>
-              <Nav.Item>News</Nav.Item>
-              <Nav.Item>Help</Nav.Item>
-            </Nav>
-            <Nav pullRight>
-              <Nav.Item icon={<Icon icon="cog" />}>Profile</Nav.Item>
-            </Nav>
-          </Navbar.Body>
-        </Navbar>
+        <NavbarComponent></NavbarComponent>
       </Header>
       <Container>
-        <Sidebar><SidebarNav></SidebarNav></Sidebar>
+        <Sidebar>
+          <SidenavComponent></SidenavComponent>
+        </Sidebar>
         <Content>{children}</Content>
       </Container>
-      <Footer className="footer">
-
-      </Footer>
+      <Footer className="footer"></Footer>
     </Container>
   );
 };
