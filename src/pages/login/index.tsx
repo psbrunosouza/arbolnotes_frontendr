8ec44699dd8ef2458a4382c487f1a5fc84c9c9b2
psbrunosouza <psbrunosouza@gmail.com>
2021-08-08 15:52:58 -0300

    visual changes in login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { Row, Col, Form, Input, Button } from "antd";
+import { Row, Col, Form, Input, Button, Checkbox, Typography } from "antd";
 
 const onSubmit = (values: any) => {
   console.log(values);
 };
 
 export default function Login() {
+  const { Title } = Typography;
+
   return (
     <>
       <Row>
         <Col className="col-form-login" span={15}>
-          <Row style={{height: '100vh'}} align="middle" justify="center">
+          <Row style={{ height: "100vh" }} align="middle" justify="center">
             <Col>
               <Form
                 name="basic"
                 initialValues={{ remember: true, password: "", email: "" }}
                 onFinish={onSubmit}
               >
+                <Row align="middle" justify="center">
+                  <Col>
+                    <Title level={2}>REDO</Title>
+                  </Col>
+                </Row>
+
                 <Form.Item name="email">
                   <Input />
                 </Form.Item>
@@ -25,9 +33,24 @@ export default function Login() {
                   <Input.Password />
                 </Form.Item>
 
-                <Form.Item >
+                <Row align="middle" justify="space-between" style={{ marginBottom: 12 }}>
+                  <Col>
+                    <Checkbox>Keep logged</Checkbox>
+                  </Col>
+                  <Col>
+                    <a href="#f">Forget?</a>
+                  </Col>
+                </Row>
+
+                <Form.Item>
                   <Button block type="primary" htmlType="submit">
                     Submit
+                  </Button>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button block type="link" htmlType="button">
+                    Create account
                   </Button>
                 </Form.Item>
               </Form>
@@ -35,7 +58,9 @@ export default function Login() {
           </Row>
         </Col>
         <Col className="col-form-image" span={9}>
-          <div></div>
+          <Row style={{height: '100vh', background: '#8282f6'}}>
+            <Col></Col>
+          </Row>
         </Col>
       </Row>
     </>
